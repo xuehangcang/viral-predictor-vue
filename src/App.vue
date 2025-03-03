a412ab15edb6ff873aaeeb120ec613ccd49f200d
XuehangCang <xuehangcang@outlook.com>
2025-03-03 23:18:06 +0800

    feat:tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,9 @@ import {RouterLink, RouterView} from 'vue-router'
 
 <!--    <RouterLink to="/">Home</RouterLink>-->
 <!--    <RouterLink to="/about">About</RouterLink>-->
-
+    <h1 class="text-3xl font-bold underline">
+        Hello world!
+    </h1>
     <RouterView/>
 </template>
 
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-
+import './assets/styles.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
