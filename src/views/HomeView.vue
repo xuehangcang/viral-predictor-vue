c0965144f7415ef450ee22fdf79d2cd322b102d4
XuehangCang <xuehangcang@outlook.com>
2025-03-05 09:25:29 +0800

    debug:json

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,7 +9,7 @@ const option = ref({
     },
     xAxis: {
         type: 'category',
-        data: [0,1,2,3,4]
+        data: [0, 1, 2, 3, 4]
     },
     yAxis: {
         type: 'value'
@@ -17,13 +17,13 @@ const option = ref({
     series: [
         {
             name: 'engagement_a',
-            data: [3,8,4,3,8],
+            data: [3, 8, 4, 3, 8],
             type: 'line',
             smooth: true
         },
         {
             name: 'engagement_b',
-            data: [7,3,4,3,6],
+            data: [7, 3, 4, 3, 6],
             type: 'line',
             smooth: true
         }
@@ -90,18 +90,17 @@ const max_users = ref(20)
 const openai_api_key = ref("")
 const model_type = ref("openai/gpt-4o")
 
-const like_confidence =ref(0)
+const like_confidence = ref(0)
 const like_winner = ref("Nan")
 
 const comment_confidence = ref(0)
 const comment_winner = ref("Nan")
 
-const share_confidence =ref(0)
+const share_confidence = ref(0)
 const share_winner = ref("Nan")
 
 const quote_confidence = ref(0)
 const quote_winner = ref("Nan")
-
 
 
 async function PredictClick() {
@@ -119,9 +118,9 @@ async function PredictClick() {
     const total_b = ref(0)
     const users = ref(0)
 
-    option.value.xAxis.data =[]
-    option.value.series[0].data =[]
-    option.value.series[1].data =[]
+    option.value.xAxis.data = []
+    option.value.series[0].data = []
+    option.value.series[1].data = []
     for (let i = 0; i < max_users.value; i++) {
         // console.log(`User ${i + 1}`);
         prompt_a.value = `You are scrolling through ${selectedPlatform.value} and came across the following content:
@@ -152,7 +151,7 @@ async function PredictClick() {
             apiKey: openai_api_key.value,
             dangerouslyAllowBrowser: true
         });
-        const tasks_a  = await openai.chat.completions.create({
+        const tasks_a = await openai.chat.completions.create({
             model: "gpt-4o-mini",
             messages: [
                 {role: "system", content: ""},
@@ -161,7 +160,7 @@ async function PredictClick() {
             response_format: {"type": "json_object"},
             store: true,
         });
-        const tasks_b  = await openai.chat.completions.create({
+        const tasks_b = await openai.chat.completions.create({
             model: "gpt-4o-mini",
             messages: [
                 {role: "system", content: ""},
@@ -170,9 +169,19 @@ async function PredictClick() {
             response_format: {"type": "json_object"},
             store: true,
         });
-        const predictions_a  = JSON.parse(tasks_a.choices[0].message.content);
-        const predictions_b  = JSON.parse(tasks_b.choices[0].message.content);
+        let predictions_a, predictions_b;
 
+        if (tasks_a.choices[0].message.content !== null) {
+            predictions_a = JSON.parse(tasks_a.choices[0].message.content);
+        } else {
+            console.error("tasks_a content is null");
+        }
+
+        if (tasks_b.choices[0].message.content !== null) {
+            predictions_b = JSON.parse(tasks_b.choices[0].message.content);
+        } else {
+            console.error("tasks_b content is null");
+        }
 
         like_a.value += predictions_a.like
         comment_a.value += predictions_a.comment
@@ -195,19 +204,19 @@ async function PredictClick() {
         const [share_winner_results, share_confidence_results] = calcConfidence(users.value, share_a.value, share_b.value)
         const [quote_winner_results, quote_confidence_results] = calcConfidence(users.value, quote_a.value, quote_b.value)
 
-        like_confidence.value = like_confidence_results
+        like_confidence.value = isNaN(like_confidence_results) ? 0 : like_confidence_results;
         like_winner.value = like_winner_results
 
-        comment_confidence.value = comment_confidence_results
+        comment_confidence.value = isNaN(comment_confidence_results) ? 0 : comment_confidence_results;
         comment_winner.value = comment_winner_results
 
-        share_confidence.value = share_confidence_results
+        share_confidence.value = isNaN(share_confidence_results) ? 0 : share_confidence_results;
         share_winner.value = share_winner_results
 
-        quote_confidence.value = quote_confidence_results
+        quote_confidence.value = isNaN(quote_confidence_results) ? 0 : quote_confidence_results;
         quote_winner.value = quote_winner_results
 
-        users.value+=1
+        users.value += 1
 
     }
 
@@ -217,91 +226,91 @@ async function PredictClick() {
 </script>
 
 <template>
-<div class="container mx-auto pt-10">
-    <h1 class="text-3xl text-white font-bold">Viral Predictor</h1>
-    <p class="text-white pt-4 font-bold">Simulate how users react to your content so you know it'll go viral
-        before you post</p>
+    <div class="container mx-auto pt-10">
+        <h1 class="text-3xl text-white font-bold">Viral Predictor</h1>
+        <p class="text-white pt-4 font-bold">Simulate how users react to your content so you know it'll go viral
+            before you post</p>
 
-    <div class="grid grid-cols-2 gap-6">
-        <div class="">
-            <h1 class="text-2xl text-white font-bold pt-10">Version A</h1>
-            <p class="text-white text-sm pt-2">Enter your content here</p>
-            <div class="pt-2">
+        <div class="grid grid-cols-2 gap-6">
+            <div class="">
+                <h1 class="text-2xl text-white font-bold pt-10">Version A</h1>
+                <p class="text-white text-sm pt-2">Enter your content here</p>
+                <div class="pt-2">
                 <textarea
                     class="bg-gray-800 pl-2 w-full h-60 rounded-lg focus:outline-none focus:ring ring-gray-500 text-white"
                     rows="4" v-model="version_a"></textarea>
+                </div>
+                <p class="text-white text-sm pt-2">Platform</p>
+                <div class="pt-2">
+                    <select
+                        class="text-white pl-2 bg-gray-800 w-full h-10 rounded-lg focus:outline-none focus:ring ring-gray-500"
+                        v-model="selectedPlatform">
+                        <option v-for="platform in platforms" :key="platform.value" :value="platform.value">
+                            {{ platform.name }}
+                        </option>
+                    </select>
+                </div>
+                <p class="text-white text-sm pt-2">OpenRouter API Key</p>
+                <div class="pt-2">
+                    <input
+                        class="bg-gray-800 pl-2 w-full h-10 rounded-lg focus:outline-none focus:ring ring-gray-500 text-white"
+                        type="text" v-model="openai_api_key">
+                </div>
+                <div class="pt-6">
+                    <button
+                        class="bg-gray-800 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-lg focus:outline-none focus:ring ring-gray-500"
+                        @click="PredictClick">
+                        Predict
+                    </button>
+                </div>
             </div>
-            <p class="text-white text-sm pt-2">Platform</p>
-            <div class="pt-2">
-                <select
-                    class="text-white pl-2 bg-gray-800 w-full h-10 rounded-lg focus:outline-none focus:ring ring-gray-500"
-                    v-model="selectedPlatform">
-                    <option v-for="platform in platforms" :key="platform.value" :value="platform.value">
-                        {{ platform.name }}
-                    </option>
-                </select>
-            </div>
-            <p class="text-white text-sm pt-2">OpenRouter API Key</p>
-            <div class="pt-2">
-                <input
-                    class="bg-gray-800 pl-2 w-full h-10 rounded-lg focus:outline-none focus:ring ring-gray-500 text-white"
-                    type="text" v-model="openai_api_key">
-            </div>
-            <div class="pt-6">
-                <button
-                    class="bg-gray-800 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-lg focus:outline-none focus:ring ring-gray-500"
-                    @click="PredictClick">
-                    Predict
-                </button>
-            </div>
-        </div>
 
-        <div class="">
-            <h1 class="text-2xl text-white font-bold pt-10">Version B</h1>
-            <p class="text-white text-sm  pt-2">Enter your content here</p>
-            <div class="pt-2">
+            <div class="">
+                <h1 class="text-2xl text-white font-bold pt-10">Version B</h1>
+                <p class="text-white text-sm  pt-2">Enter your content here</p>
+                <div class="pt-2">
                 <textarea
                     class="bg-gray-800 pl-2 w-full h-60 rounded-lg focus:outline-none focus:ring ring-gray-500 text-white"
                     rows="4" v-model="version_b"></textarea>
-            </div>
-            <p class="text-white text-sm  pt-2">Max Users</p>
-            <div class="pt-2">
-                <input
-                    class="bg-gray-800 pl-2 w-full h-10 rounded-lg focus:outline-none focus:ring ring-gray-500 text-white"
-                    type="text" v-model="max_users">
-            </div>
-            <p class="text-white text-sm  pt-2">Model</p>
-            <div class="pt-2 ">
-                <input
-                    class="bg-gray-800 pl-2 w-full h-10 rounded-lg focus:outline-none focus:ring ring-gray-500 text-white"
-                    type="text" v-model="model_type">
+                </div>
+                <p class="text-white text-sm  pt-2">Max Users</p>
+                <div class="pt-2">
+                    <input
+                        class="bg-gray-800 pl-2 w-full h-10 rounded-lg focus:outline-none focus:ring ring-gray-500 text-white"
+                        type="text" v-model="max_users">
+                </div>
+                <p class="text-white text-sm  pt-2">Model</p>
+                <div class="pt-2 ">
+                    <input
+                        class="bg-gray-800 pl-2 w-full h-10 rounded-lg focus:outline-none focus:ring ring-gray-500 text-white"
+                        type="text" v-model="model_type">
+                </div>
             </div>
         </div>
-    </div>
-    <h1 class="text-2xl text-white font-bold pt-10">Cumulative Engagement</h1>
+        <h1 class="text-2xl text-white font-bold pt-10">Cumulative Engagement</h1>
 
-    <div class="">
-        <v-chart class="" autoresize :option="option" theme="dark" style="height: 400px"></v-chart>
-    </div>
+        <div class="">
+            <v-chart class="" autoresize :option="option" theme="dark" style="height: 400px"></v-chart>
+        </div>
 
-    <h1 class="text-2xl text-white font-bold">Statistical Confidence</h1>
-    <div class="grid grid-cols-4 gap-6 pt-4 pb-10">
-        <div class="text-white">
-            <p class="pt-2 font-bold text-xl">Likes</p>
-            <p class="pt-2">{{ like_confidence.toFixed(2) }}% Vers.{{ like_winner }}</p>
-        </div>
-        <div class="text-white">
-            <p class="pt-2 font-bold text-xl">Comments</p>
-            <p class="pt-2">{{ comment_confidence.toFixed(2) }}% Vers.{{ comment_winner }}</p>
-        </div>
-        <div class="text-white">
-            <p class="pt-2 font-bold text-xl">Shares</p>
-            <p class="pt-2">{{ share_confidence.toFixed(2) }}% Vers.{{ share_winner }}</p>
-        </div>
-        <div class="text-white">
-            <p class="pt-2 font-bold text-xl">Quotes</p>
-            <p class="pt-2">{{ quote_confidence.toFixed(2) }}% Vers.{{ quote_winner }}</p>
+        <h1 class="text-2xl text-white font-bold">Statistical Confidence</h1>
+        <div class="grid grid-cols-4 gap-6 pt-4 pb-10">
+            <div class="text-white">
+                <p class="pt-2 font-bold text-xl">Likes</p>
+                <p class="pt-2">{{ like_confidence.toFixed(2) }}% Vers.{{ like_winner }}</p>
+            </div>
+            <div class="text-white">
+                <p class="pt-2 font-bold text-xl">Comments</p>
+                <p class="pt-2">{{ comment_confidence.toFixed(2) }}% Vers.{{ comment_winner }}</p>
+            </div>
+            <div class="text-white">
+                <p class="pt-2 font-bold text-xl">Shares</p>
+                <p class="pt-2">{{ share_confidence.toFixed(2) }}% Vers.{{ share_winner }}</p>
+            </div>
+            <div class="text-white">
+                <p class="pt-2 font-bold text-xl">Quotes</p>
+                <p class="pt-2">{{ quote_confidence.toFixed(2) }}% Vers.{{ quote_winner }}</p>
+            </div>
         </div>
     </div>
-</div>
 </template>
